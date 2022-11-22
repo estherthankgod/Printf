032b4e56baa37edf46fa86f8942c984bf8826fe5
estherthankgod <tripcy18@gmail.com>
2022-11-22 14:34:29 -0800

    main.h

--- main.h
+++ main.h
@@ -45,7 +45,7 @@ int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
 
-/****** FUNCTIONS ******/
+/****************** FUNCTIONS ******************/
 
 /* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
@@ -110,7 +110,7 @@ int write_unsgnd(int is_negative, int ind,
 char buffer[],
 int flags, int width, int precision, int size);
 
-/****** UTILS ******/
+/****************** UTILS ******************/
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
